cbd339ec95c49ef07fb94de03e723db21c1f57c0
keselme90 <keselme90@gmail.com>
2019-06-21 14:12:37 +0300

    commented out logger usage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const logger = require('./middleware/logger')
+// const logger = require('./middleware/logger')
 
 const app = express()
 
